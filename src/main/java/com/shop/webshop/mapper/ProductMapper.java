588b2e56a16e890e476566afd81c9188d6891f04
MihalyMatyas <mihamatyas@gmail.com>
2022-03-09 20:25:36 +0200

    .

---
+++ src/main/java/com/shop/webshop/mapper/ProductMapper.java
@@ -0,0 +1,36 @@
+package com.shop.webshop.mapper;
+
+import com.shop.webshop.dto.productdto.ProductCreateDto;
+import com.shop.webshop.dto.productdto.ProductFullDto;
+import com.shop.webshop.model.Product;
+
+public class ProductMapper {
+    public static Product productToEntity(ProductCreateDto productCreateDto) {
+
+        Product product = new Product();
+        product.setName(productCreateDto.getName());
+        product.setDescription(productCreateDto.getDescription());
+        product.setThumbnail(productCreateDto.getThumbnail());
+        product.setCategory(productCreateDto.getCategory());
+        product.setPrice(productCreateDto.getPrice());
+        product.setProductType(productCreateDto.getProductType());
+        product.setStock(productCreateDto.getStock());
+
+        return product;
+    }
+
+    public static ProductFullDto productToFullDto(Product product) {
+        ProductFullDto productFullDto = new ProductFullDto();
+        productFullDto.setId(product.getId());
+        productFullDto.setName(product.getName());
+        productFullDto.setDescription(product.getDescription());
+        productFullDto.setThumbnail(product.getThumbnail());
+        productFullDto.setCategory(product.getCategory());
+        productFullDto.setPrice(product.getPrice());
+        productFullDto.setProductType(product.getProductType());
+        productFullDto.setStock(product.getStock());
+
+        return productFullDto;
+    }
+
+}
