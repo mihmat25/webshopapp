b4652fa9c32a054ca4b5118fac99359b3cb23b2e
Alexandra Capra <rusu.alexandra30@yahoo.com>
2022-03-11 22:13:27 +0200

    mapper

--- src/main/java/com/shop/webshop/mapper/CategoryMapper.java
+++ src/main/java/com/shop/webshop/mapper/CategoryMapper.java
@@ -5,14 +5,14 @@ import com.shop.webshop.dto.productdto.CategoryFullDto;
 import com.shop.webshop.model.Category;
 
 public class CategoryMapper {
-    public static Category category(CategoryCreateDto categoryCreateDto) {
+    public static Category categoryToEntity(CategoryCreateDto categoryCreateDto) {
         Category category = new Category();
         category.setName(categoryCreateDto.getName());
 
         return category;
     }
 
-    public static CategoryFullDto categoryFullDto(Category category) {
+    public static CategoryFullDto categoryToFullDto(Category category) {
         CategoryFullDto categoryFullDto = new CategoryFullDto();
         categoryFullDto.setName(categoryFullDto.getName());
 
